ac4f84db7ce5d88181ac92ca71bf6a3163c01ad4
tommy2tonez <tom.p.nguyen@outlook.com>
2024-10-31 21:51:03 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #define DEBUG_MODE_FLAG true 
 
-#include <iostream>
-
-// #include "network_memlock_proxyspin.h"
-// #include "network_memlock.h"
-// #include <type_traits>
-// #include <utility>
-// #include <functional>
-#include "network_uma_tlb_impl1.h"
-#include "network_uma_tlb.h"
-
 int main(){
 
+    //Remember
+    //first - linear is not numerically stable - this is to be researched
+    //second - linear does not uniformly distribute the context - unless it is one dimensional x two dimensional linear opeartion
+    //the drawbacks to one_dimensional x two_dimensional linear is that the possibility space is too big - this leads to remembering the result rather than actually intelligently connecting the result
+    //that's why two_dimensional x two_dimensional linear is preferred - yet it does not uniformly distribute the context - this is bad
+    //the balance is to be found
+    //people tried to use transpose(0, 1) and rotary embeddings to actually solve that without realizing that 
+    //uniform context distribution could be briefly described as given two random logits, the influence of a logit on another is uniformly distributed among all possible pairs after a certain number of tranformations
+    //all there is to AGI is that - the dimensional reduction - the possiblity space - the uniform distribution - the numerical stable model - the dynamic transformation path
 }
