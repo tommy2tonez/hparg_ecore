5f11a359e4899bdc42e5ed3df68584ca3d9b8223
tommy2tonez <tom.p.nguyen@outlook.com>
2024-11-13 01:01:43 -0500

    update

--- src/stdx.h
+++ src/stdx.h
@@ -18,39 +18,6 @@
 
 namespace stdx{
 
-    //I need to make sense of the compiler definition of launder, and the std definition of launder
-    //and actually implement a version that actually intersects the logic
-    
-    //compiler only defined usage of launder:
-    //convert type A * to type B * - such that pointer reachability of A is the same as pointer reachability of B
-    //and A * and B * coexist at the same address
-    //exists as if defined by the std lifetime rules
-    //the result of launder must not outlive its argument
-
-    //std defined usage of launder:
-    //convert type A * to type B *
-    //A * and B * coexists at the same address
-    //pointer reachability of B <= pointer reachability of A
-    //the result of launder must not outlive its argument
-    //only used for devirtualization fence
-    //such that an access to a polymorphic function requires a read of the polymorphic header associated with the calling function - before returning result 
-    //in this case - ptr() is a property of polymorphic_launderer
-    //which is aliased to launder<uint8_t>::ptr, launder<uint8_t>::uint16_t, launder<uin32_t>::ptr, launder<uint64_t>::ptr, launder<void *>::ptr, etc.
-
-    //assume that the compiler exit point is a table read (such table is only used for returning results - not inlining calling functions)
-    //then the usage is defined
-    //because a force of the polymorphic read turns void * -> runtime-defined
-    //and the probabilistic of void * being uint8_t *, uint16_t *, uint32_t * is then uniformly distributed
-    //so void * assumptions post the table read must be the ones that satisfy aliasing-rules with all the uint8_t *, uint16_t *, etc.
-    //so the void * post the table read is clean  
-
-    //assume that the compiler does malicious thing and inline caller -> table_dispatch
-    //then the usage of const void * or void * is defined
-    //and the const void * or void * must be cleansed according to the launder logic
-
-    //the only compiler that adheres to the std is, unsurprisingly, GCC
-    //clang ignores the polymorphic read and makes malicious optimization
-
     struct polymorphic_launderer{
         virtual auto ptr() noexcept -> void *{
             return nullptr;
@@ -278,7 +245,7 @@ namespace stdx{
         }
     };
 
-    static inline constexpr bool IS_SAFE_MEMORY_ORDER_ENABLED       = true; 
+    static inline constexpr bool IS_SAFE_MEMORY_ORDER_ENABLED       = true;
     static inline constexpr bool IS_SAFE_INTEGER_CONVERSION_ENABLED = true;
 
     template <class Lock>
@@ -390,6 +357,7 @@ namespace stdx{
     //as-of gcc-14 - this launder works as if ptr is unique_ptr<void * __restrict__> and the scope of usage is the same as the callee scope
     //the implementation is defined, according to STD - this is not a Russian hack
     //extra instructions were introduced (std::atomic_signal_fence() + void * volatile) to be gcc-compiler-compliant
+    //some random commentor mentioned that this should be a seq_cst transaction to hinder inlining things -> launder dispatch table
 
     template <class T>
     inline __attribute__((always_inline)) auto launder_pointer(void * volatile ptr) noexcept -> T *{
@@ -401,8 +369,10 @@ namespace stdx{
         std::atomic_signal_fence(std::memory_order_seq_cst);
         launderer<void> launder_machine{};
         launder_machine.value = ptr;
-
-        return static_cast<T *>(std::launder(&launder_machine)->ptr()); //this is a correct way of laundering defined by the std - force ptr() to read the polymorphic header
+        T * rs = static_cast<T *>(std::launder(&launder_machine)->ptr());
+        std::atomic_signal_fence(std::memory_order_seq_cst);
+        
+        return rs;
     }
 
     template <class T>
@@ -415,8 +385,10 @@ namespace stdx{
         std::atomic_signal_fence(std::memory_order_seq_cst);
         const_launderer<void> launder_machine{};
         launder_machine.value = ptr;
+        const T * rs = static_cast<const T *>(std::launder(&launder_machine)->ptr());
+        std::atomic_signal_fence(std::memory_order_seq_cst);
 
-        return static_cast<const T *>(std::launder(&launder_machine)->ptr()); //this is a correct way of laundering defined by the std - force ptr() to read the polymorphic header
+        return rs;
     }
 
     template <class Destructor>
