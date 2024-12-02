8528f65d805fa7560509812632ec4b20e697fba1
tommy2tonez <tom.p.nguyen@outlook.com>
2024-12-02 05:08:40 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -24,5 +24,10 @@ int main(){
     //and use SIMD approach to do key cmp
     //somewhat a mixed of swiss table and our implementation - not chaining - because there is no SIMD in chaining
     //we want to minimize cache miss by not dereferencing the "intermediate" buckets  
+    //how do we do that precisely - okay - this is hard
+    //we know that the hashing function - normally - for linear probing uniformly distribute the hash_range
+    //everytime we read - we read 1 cache_line - so we want to reduce our hash_to_index_range by a factor of CACHE_LINE_SZ / size(address)
+    //so we hash the key -> spit to index -> re-multiply to get the region -> read one cache line -> do SIMD directly on the read region to find the key -> extract address and return the bucket
+    //with this approach - we, hopefully, can reduce our lookup time by a factor of 2 and make this, truly, the fastest flat_hash_table on Earth
 
 }
