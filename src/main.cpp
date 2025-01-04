d63e0af166c14c7e2a890c05ef9f8545d51279a5
tommy2tonez <tom.p.nguyen@outlook.com>
2025-01-03 23:52:25 -0500

    update

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+//alrights fellas 
+//today instead of doing planes and coordinates - we are doing spheres and radians - then we'll come up with synthetic function that's best at approximating things - we'll work on this for a month or two - then we'll approxmiate the function by using math_approx
+//we'll be rich guys, be patient please
+//consider this plane eqn: normal vector . <x - x0> <y - y0> <z - z0> = 0
+//= ax + by + cz + ax0 + by0 + cz0 = 0
+//linear is done by doing plane projections - which is fine - but not very good at retaining informations
