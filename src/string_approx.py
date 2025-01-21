e063f0255ad680f140dc8fcf355091df3f351b82
tommy2tonez <tom.p.nguyen@outlook.com>
2025-01-21 00:48:24 -0500

    update

---
+++ src/string_approx.py
@@ -0,0 +1,36 @@
+
+#alright - this is string approx - string approx problems can be defined as - stretch horizontally, stretch vertically, periodically (this is 1 - many linear (gemm)), and calibration
+
+#let's build a tree of string operations
+#string operation is essentially massful object interference on the surface - to pull the surface towards the massful object 
+#let's assume our base is f(x) = 0, we have 1000 points on the surface - each massful object is going to pull the points above or below the surface (towards the massful object's coordinate)
+#we want to recursively build our continuous function by this formula
+
+#f(x) = f_previous(x) + string_interference(f_base, massful_objects)
+#p(x) = string_interference(f(x), massful objects) 
+
+#let's get to the root of intelligent semantic space - intelligent semantic space is calibration + locality compression (continuity) + periodic (periodic by dimensional reduction)
+#we aren't doing random semantic space
+#alright - you have a continuous function f(x) = x^2 (string) - it does not mean that it has square relation on the training data f(x) and x - but it is in some coordinate
+#so it's important to do calibration (which is p(x) function) of coordinate as we are doing training - this is an oversimplification
+#assume we have 20 base strings that make up the projection space 
+#the first string will get calibrated 19 times (via the usage of massful objects)
+#the second string will get calibrated 18 times
+#the base string is our arbitrary "calibrated coordinate"
+
+#the art of compression is machine learning - yet I think we are not there - the calibration part and the periodic - we got the base string correctly
+#we need to work on the theories
+
+#gravity is probably on the 20th string
+#electromagnetic is probably on the 21th string
+#we call string a multi-dimensional projection shape 
+
+#on 2D euclidean coordnate - we have particle projection - and we have wave-like projection which is a 2 dimensional string
+#thing is we want to use God's favorite language of "massful" for continuity compression
+#what Einstein described was 4 dimensional string - and massful object would bend continuity of the 4 dimensional projection string
+#Einstein was not wrong - just not sufficient - in the sense of information flux
+#information flux is the maximum information that an enclosed space can receive from outer space (which is kinda maximum pingpong/s)
+#string quantum is right - but we can't observe another world to tell our world - the photon that went through the slit and the observed photon aren't on the same coordinate - they are on 20 dimensional projection string - and they went different direction  
+
+def main():
+    pass 
