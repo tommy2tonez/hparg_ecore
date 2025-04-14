ce16572cd8e72cd40747a600960c4ca568b44910
tommy2tonez <tom.p.nguyen@outlook.com>
2025-04-13 20:52:35 -0400

    update

--- src/network_fileio_chksum_x.h
+++ src/network_fileio_chksum_x.h
@@ -13,57 +13,13 @@
 
 namespace dg::network_fileio_chksum_x{
 
-    //improve error_code return - convert the errors -> RUNTIME_FILEIO_ERROR for generic purpose 
-
-    //1 success write == guarantee the next success read to read the success write, this is the sole contract that we provide
-    //unified_fsys_io would keep track of the contract status, the only thing that is wrong is the metadata header of the unified_fsys_io
-    //alright, this should suffice
+    //socket is about 1 transmit == max one recv
+    //file is about 1 success write == next success read is the inverse operation of the write
     
-    //let's see what we are trying to do
-
-    //we have a buffer of <char *, size_t>
-    //legacy fsys: 1 success write == nasal demon
-    //                             == sometimes correct inverse operation read, sometimes incorrect, sometimes modified the file, sometimes the file disappeared out of thin air
-    //chksum_x: 1 success write == very likely  (chances better than RAM) good success read, which means the next success read would return the success write data, does not mean that the next success read means nasal demon
-    //unified_x: increase the chances of success write and next success read by using multiple data_sources 
-
-    //we dont care about not success write, not success write could mean everything, from modifying the file, to changing the file size -> 0, -> oo
-    //we dont care about ACID property either, such is we are assuming that the storage is invalidated if the power is off, or gammar beta ray RAM attacks
-    //ACID is storage's business, we aren't doing storage, we are doing forward + backward
-    //not a lot of people understand this
-    //I mean 99% of people dont understand this, the contract of 1 success write, and the next success read means the inverse operation of the write
-    //alright, we need to add support for unordered_map metadata on RAM
-    //because the fsys is nasal demon, we have absolutely no guarantee that the read file is on cache, and a cache flush to disk is written later on 
-    //disk might block all the cache flush and return the previous tx, which is also considered a valid read
-
-    //we have covered 99.99999999999% of cases
-    //our fault tolerance is probably at 10 ** 18 bytes/ error
-    //far from our RAM fault
-    //its complicated, really is
-    //fsys is a very bad place to work on
-    //alright, let's strategize 
-    //we know for sure that a fsys read would issue at least 1 memory ordering on the kernel side, this is unavoidable
-    //so it's fine to actually build a distributed unordered_map to store the metadata_fsys
-    //we now face with the problem of how big the map should be?
-    //is it a temporal unordered map or replace map, only to reduce the statistical chances, not to bring the fault rate -> RAM rate
-    //after all, we are messing with statistics, why use an absolute instrument when there is actually nothing really is absolute  
-    //let's stick with the replace map, we are to load std::memory_order_relaxed of std::array<relaxable_sz, char>
-    //remember, we are to mess with the statistics, we are to return as many failed read() as possible
-    //we load the relaxed in chunks, because we are messing with statistics 
-    //the notsynchronizedchance of relaxed is included in the chances of corrupted read()
-    //we are using one nasal demon to fight with another nasal demon
-    //the two have to agree with each other, this is the punch line
-
-    //this is defined piece of code
-    //assume that the fsys nasal demon is incorrect, our map nasal demon is correct (out)
-    //                                    incorrect, incorrect (another nasal demon, the chances of this guy to happen is close to 0, matched fp, matched chksum, alright, we need to specialize chksum, we dont go there yet)
-    //                                    correct, correct (in)
-    //                                    correct, incorrect (out)
-    //whatever, we'll move on with the implementation
-
-    //I've run numbers, it chances better than RAM to have a success read
-    //we are probably risking more false negatives than necessary to get a true positive
-    //it's complex
+    //we need to improve the hash_map, it's complicated, the memory_order_acquire + memory_order_release would void the value of our fileio_chksum_x 
+    //the hash_map is only good when used with the unified_fileio, and the unified_io does the business of multiple buckets to avoid the false negative filter of the chksum_x
+    //remember, the chksum_x responsibility is to filter as many as possible, as long as the true positive rate is high and false positive rate is low
+    //we can have false negatives, that's irrelevant
 
     struct FileHeader{
         std::pair<uint64_t, uint64_t> chksum;
