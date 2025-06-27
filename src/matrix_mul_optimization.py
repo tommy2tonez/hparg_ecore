cabd4d4d75b0b9e0a5894d95d05d987e768b9c75
tommy2tonez <tom.p.nguyen@outlook.com>
2025-06-27 12:53:55 +0700

    update

--- src/matrix_mul_optimization.py
+++ src/matrix_mul_optimization.py
@@ -113,6 +113,8 @@ def generic_sum(x_list: list[Logit], projection_storage_sz: int) -> Logit:
 #two dimensions     == two binary tree of two sums
 #three dimensions   == ...
 
+#this two_shake operation is very important, because every entry in the logit_list is actually a unit, we have yet to tell if this is 1 logit or 1 tile
+
 def two_shake(logit_list: list[Logit], projection_storage_sz: int, iteration_sz: int) -> list[Logit]:
 
     list_sz: int = len(logit_list)
@@ -131,26 +133,37 @@ def two_shake(logit_list: list[Logit], projection_storage_sz: int, iteration_sz:
     two_dimensional_logit_list: list[list[Logit]]               = shape_as(logit_list, [dim_sz, dim_sz])
     transformed_two_dimensional_logit_list: list[list[Logit]]   = []
 
+    #our generic formula's gonna look like this
+    #we need to improve the twosum + threesum + foursum operation (we are only focusing on the finite finite Taylor Series patterns, we aren't looking in the direction of finite infinite yet)
+
     for i in range(dim_sz):
         feature_vec: list[list[Logit]]  = list()
         shaked_row: list[Logit]         = two_shake(two_dimensional_logit_list[i], , iteration_sz)
 
         for j in range(dim_sz):
             accumulated_logit: Logit            = twosum_binary_tree_accumulate(shaked_row) #uniform rule for accumulation, in other words, we can reuse the rules for other rows (or cols), we can use threesums or foursums to reduce the tree height which would help with the context loss (we can argue otherwise if we can prove that if the two_shake rs is uniformly distributed in an arbitrary space, we can continue such properties)
+                                                                                            #the problem is that we can't assume that, because logit density of projection_storage_sz might not allow that, so threesum or foursum to reduce the burden of the two_shake() is a necessity
+
             another_accumulated_logit: Logit    = twosum_binary_tree_accumulate(shaked_row) #uniform rule for accumulation
 
-            feature_vec.append([accumulated_logit, another_accumulated_logit])
+            feature_vec.append([accumulated_logit, another_accumulated_logit]) #this is very important, we can argue that another rotate would fix this problem but we are logically wrong, the context of the input is already altered by then, this would fix the problem of immediate representation of row
 
         transformed_two_dimensional_logit_list.append(feature_vec)
 
     transformed_two_dimensional_logit_list  = rotate(shape_as(transformed_two_dimensional_logit_list, [dim_sz, dim_sz]))
-    transformed_two_dimensional_logit_list  = [two_shake(flatten(feature_vec), , iteration_sz) for feature_vec in transformed_two_dimensional_logit_list] #
+    transformed_two_dimensional_logit_list  = [two_shake(flatten(feature_vec), , iteration_sz) for feature_vec in transformed_two_dimensional_logit_list] #this is not a smaller square, which can either do not have a recursive base or not a square
 
     flattened_transformed_list: list[Logit] = flatten(transformed_two_dimensional_logit_list)
     rs_list: list[Logit]                    = list()
 
     for i in range(list_sz):
-        new_logit: Logit = three_sum(logit_list[i], flattened_transformed_list[i * 2], flattened_transformed_list[i * 2 + 1]) #alright, we are doing threesum because dimensional reduction of row, col -> 1 dimension is unstable, so we'd have to project that -> 2 dimensions, each row now contains a fuzzy representation of the entire matrix
+        #we are not exactly wrong, because the twoshake of the row would polymorphic the output, yet I'd want to do another accumulation
+        #the problem of machine learning is precisely that, we can assume that two_shake would "polymorphic" every entry given the matrix or we can assume that it is a set up for the twosum_binary_tree_accumulate 
+        #alright, we are doing threesum because dimensional reduction of row, col -> 1 dimension is unstable, so we'd have to project that -> 2 dimensions, each row now contains a fuzzy representation of the entire matrix
+
+        # accumulated_logit: Logit    = twosum_binary_tree_accumulate(transformed_two_dimensional_logit_list[i])
+
+        new_logit: Logit = three_sum(logit_list[i], flattened_transformed_list[i * 2], flattened_transformed_list[i * 2 + 1])
         rs_list.append(new_logit)
 
     return two_shake(rotate(rs_list), , iteration_sz - 1)
